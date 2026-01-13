6b2b3da570ef340c6f4ccb551751957dbf292734
dhana deshik <pdhanadeshik2889@gmail.com>
2026-01-13 05:52:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8303501508-dbebb",
+  "appId": "1:86561803816:web:d6fa1ba26466e9da1baf1d",
+  "apiKey": "AIzaSyCL-Do9baBVrmNGsOSXHB3V8h3us_gBTEI",
+  "authDomain": "studio-8303501508-dbebb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "86561803816"
+};
